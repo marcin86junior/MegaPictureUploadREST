5d4c48cc52127cc80e0da3231e0b84c3ebd7da34
marcin86junior <marcin86.design@gmail.com>
2022-06-19 08:57:22 +0200

    hotfix for docker media folder -> working in local and docker

--- django_rest_imageupload_backend/django_rest_imageupload_backend/settings.py
+++ django_rest_imageupload_backend/django_rest_imageupload_backend/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 import os
-MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'uploaded_media')
+MEDIA_ROOT = os.path.join(BASE_DIR, '/uploaded_media')
 MEDIA_URL = '/media/'
 
 REST_FRAMEWORK = {
